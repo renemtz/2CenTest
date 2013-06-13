2af445dffbcea2e8d46852ad80e062cef62012ad
René Mtz <rene9111@gmail.com>
2013-06-12 23:42:37 -0500

    agregados atributos a ciclo

--- grails-app/domain/com/ulsa/evaluacion/Ciclo.groovy
+++ grails-app/domain/com/ulsa/evaluacion/Ciclo.groovy
@@ -1,7 +1,16 @@
 package com.ulsa.evaluacion
 
 class Ciclo {
+	String clave
+	String nombre
+	String descripcion
 
     static constraints = {
+		clave nullable:false, blank: false
+		nombre nullable:false
     }
+	
+	String toString() {
+		"${clave}"
+	}
 }
