d19de47f39a68d5efb82fbd25a83992e29651409
René Mtz <rene9111@gmail.com>
2013-06-13 00:22:14 -0500

    agregadas relaciones

--- grails-app/domain/com/ulsa/evaluacion/Ciclo.groovy
+++ grails-app/domain/com/ulsa/evaluacion/Ciclo.groovy
@@ -4,6 +4,8 @@ class Ciclo {
 	String clave
 	String nombre
 	String descripcion
+	
+	static hasMany = [grupos: Grupo, materias: Materia]
 
     static constraints = {
 		clave nullable:false, blank: false
